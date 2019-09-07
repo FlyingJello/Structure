c92a8a76d4396b9fb6891f19b92ba39c4a4196dc
Hugues Trottier <hugues_trottier@hotmail.com>
2019-09-07 10:31:55 -0400

    draw squares

--- structure.js
+++ structure.js
@@ -1,4 +1,50 @@
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Square {
+  constructor(x, y, mainColor, offColor) {
+    this.pos = new Point(x, y);
+    this.mainColor = mainColor;
+    this.offColor = offColor;
+  }
+
+  rotate() {
+    return true;
+  }
+
+  draw(context) {
+    context.fillStyle = this.mainColor;
+    context.fillRect(this.pos.x * shapeWidth, this.pos.y * shapeWidth, shapeWidth, shapeWidth);
+  }
+}
+
+const width = height = 5;
+const windowSize = 800;
+const shapeWidth = windowSize / width;
+
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-ctx.fillStyle = "#ec4074";
-ctx.fillRect(0, 0, 800, 800);
+
+createBoard();
+draw(ctx);
+
+function createBoard() {
+  shapes = [...Array(width).keys()].map(x => Array(height));
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      shapes[i][j] = new Square(i, j, "#74e448", "#000000");
+    }
+  }
+}
+
+function draw(context) {
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      shapes[i][j].draw(context);
+    }
+  }
+}
